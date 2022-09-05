df9114c620c145ca4ea9c65f64c45f0aacea0f58
jah <celt8@yandex.ru>
2022-09-05 10:50:21 +0500

    - hm9

---
+++ src/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String name;
+    private Author author;
+    private int year;
+    Book(String name, Author author, int year){
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+       public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Author kreig = new Author("Крейг","Уоллс");
+        Author shild = new Author("Гербет","Шилд");
+
+        Book jav = new Book("Полное руководство", shild, 2011);
+        Book spring = new Book("Spring в действии", kreig, 2005);
+
+        spring.setYear(2020);
+
     }
 }
